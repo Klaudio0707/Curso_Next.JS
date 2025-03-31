530c9b8d86257b325ec0318e8ec9b86845fa2b1c
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-03-30 23:35:43 -0300

    pratica com o use server do next

--- diretorio/src/app/page.tsx
+++ diretorio/src/app/page.tsx
@@ -1,12 +1,44 @@
 
+interface DataPropts{
+  id: number;
+  name: string;
+  full_name: string;
+  language: string;
+  owner:{
+    login: string;
+    id: number;
+    avatar_url: string;
+  }
 
-export default function Home() {
+
+}
+
+async function getData() {
+  // https://api.github.com/users/klaudio0707/repos
+  // https://api.github.com/projects/:id/repository/commits/:sha/sequence
+  const repositorios = await fetch("https://api.github.com/users/klaudio0707/repos");
+  return repositorios.json();
+}
+
+export default async function Home() {
+const data: DataPropts[] = await getData();
+
 
 return (
   <main>
     <h1>Home</h1>
   <p>Sejá bem vindo a pagina home</p>
-
+<br/>
+<h3>Meus repositorios</h3>
+{data.map((item) => (
+  <div key={item.id}>
+    <strong><p>Repositorio: {item.full_name}</p></strong>
+    <p>{item.language}</p>
+    <p>{item.owner.login}</p>
+    <p>{item.owner.id}</p>
+    <p>{item.owner.avatar_url}</p>
+  </div>
+))}
   </main>
 
 )
