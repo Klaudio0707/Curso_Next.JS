f8d9cee31309838c411a7821e760ec3ae661162c
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-03-30 22:20:53 -0300

    projeto chamado  diretorio p/ conhece o uso de components server ou client

---
+++ diretorio/src/app/contato/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useEffect, useState } from "react"
+
+export default function Contato() {
+    const [user, setUser] = useState("Cláudio")
+    return (
+    <h1>
+
+        <h1>Paginas de contato</h1>
+        <span>Telefone </span>
+    </h1>
+
+
+    )
+
+
+}
