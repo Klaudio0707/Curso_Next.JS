4f6524a92c2027917e99693412c0f27316564050
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-04-02 22:45:55 -0300

    criação do componente para use client e criaçao do botoa show

--- diretorio/src/components/OwnerRepo/index.tsx
+++ diretorio/src/components/OwnerRepo/index.tsx
@@ -1,5 +1,5 @@
-
-
+"use client"
+import { useState } from 'react';
 import Image from 'next/image'
 
 interface OwnerRepoProps{
@@ -9,15 +9,23 @@ name: string;
 }
 
 export function OwnerRepo({avatar_url, name }: OwnerRepoProps) {
-return (
+    const [show, setShow] = useState(false);
+    return (
     <div>
-        <Image
-        src={avatar_url}
-        alt='Foto usuario'
-        width={24}
-        height={24}
-        />
+        {show && (
+            <>
+            <Image
+            src={avatar_url}
+            alt='Foto usuario'
+            width={24}
+            height={24}
+            />
         <strong>{name}</strong>
+            </>
+        )}
+        <button onClick={() => setShow(!show)}>
+            {show ? "Ocultar nome": "Mostrar nome"}
+        </button>
     </div>
 
 
